25dd5b20dab13a173d77e22dc034fc1a130be605
unknown <dtumkursreeniva@DTUMKURSREEN-EB.solarcity.local>
2015-12-21 22:24:32 -0800

    Latest Code

--- PythonTest.py
+++ PythonTest.py
@@ -2,11 +2,35 @@ from slacker import Slacker
 from jenkinsapi.jenkins import Jenkins
 
 class jenkins:
-	def __init__(self,url):
-		self.url = url
-	def jenkinsServerName(self):
-		server = Jenkins(self.url)
-		return server
+	def __init__(self,url,job):
+		self.url = Jenkins(url)
+		self.job = job
+	
+	def jenkinsVersion(self):
+		J = self.url
+		print J.version
+	
+	def listofJobs(self):
+		print self.url.keys()
 
-jenkins = jenkins("http://scit-i-ec76021a:8080/")
-print jenkins.jenkinsServerName()
+	
+	def getlastgoodbuild(self):
+		print "The current Job is",self.url[self.job]
+		job = self.url[self.job]
+		print "The current job's last build ", job.get_last_build()
+		print "The current job's Good Build is ",job.get_last_good_build()
+
+	def isBuildGood(self):
+		job = self.url[self.job]
+		print job.is_good()
+
+
+		
+			
+		
+if __name__ == '__main__':
+	jenkins = jenkins("http://scit-i-ec76021a:8080/","Test_My_Solar_City_15_12_C")
+	print jenkins.jenkinsVersion() 
+	print jenkins.listofJobs()
+	print jenkins.getlastgoodbuild()
+	print jenkins.isBuildGood()
